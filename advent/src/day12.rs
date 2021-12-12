6a779806969cd14c56944eb7f07b17b6011c8fbd
Chris Parks <christopher.daniel.parks@gmail.com>
2021-12-12 13:39:35 -0800

    Precompute node kind, store graph data in (mostly) fixed sized arrays
    before:
    * day12::bench::part1:   8,119,242 ns/iter (+/- 826,080)
    * day12::bench::part2: 222,506,008 ns/iter (+/- 9,623,249)
    
    after:
    * day12::bench::part1:     675,704 ns/iter (+/- 86,175)
    * day12::bench::part2:  19,017,005 ns/iter (+/- 1,542,412)

--- advent/src/day12.rs
+++ advent/src/day12.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 /// Count paths through caves. Small caves can only be visited once.
 pub fn part1(lines: &[String]) -> usize {
     let graph = parse_graph(lines);
@@ -15,26 +13,22 @@ pub fn part2(lines: &[String]) -> usize {
 
 /// Count paths through graph from `start` to `end`. Final argument indicates
 /// whether a single small cave can be visited twice or not.
-pub fn count_paths(graph: &HashMap<&str, Vec<&str>>, mode: Mode) -> usize {
+pub fn count_paths<'a>(graph: &'a Graph<'a>, mode: Mode) -> usize {
     let mut path = Path::new();
     let twice = match mode {
         Mode::Once => true,
         Mode::Twice => false,
     };
-    walk(graph, &mut path, twice, "start")
+    walk(graph, &mut path, twice, graph.start)
 }
 
-fn walk<'a>(
-    graph: &HashMap<&str, Vec<&'a str>>,
-    path: &mut Path<'a>,
-    mut twice: bool,
-    node: &'a str,
-) -> usize {
-    if node == "end" {
+fn walk<'a>(graph: &'a Graph<'a>, path: &mut Path, mut twice: bool, node: usize) -> usize {
+    let kind = graph.kind(node);
+    if kind == Kind::End {
         return 1;
     }
 
-    if node == node.to_lowercase() && path.occurrences(node) > 0 {
+    if kind == Kind::Small && path.occurrences(node) > 0 {
         if twice {
             return 0;
         }
@@ -43,14 +37,103 @@ fn walk<'a>(
 
     path.push(node);
 
-    let paths = graph[node]
-        .iter()
-        .filter(|&&child| child != "start")
-        .map(|child| walk(graph, path, twice, child))
+    let paths = graph
+        .neighbors(node)
+        .filter(|neighbor| graph.kind(**neighbor) != Kind::Start)
+        .map(|neighbor| walk(graph, path, twice, *neighbor))
         .sum();
 
     path.pop();
     paths
+}
+
+#[derive(Debug)]
+/// Specialized graph for this input.
+pub struct Graph<'a> {
+    len: usize,
+    start: usize,
+    end: usize,
+    verts: [&'a str; 32],
+    edges: [Vec<usize>; 32],
+    kinds: [Kind; 32],
+}
+
+impl<'a> Graph<'a> {
+    /// Create empty graph.
+    pub fn new() -> Self {
+        const EMPTY: Vec<usize> = Vec::new();
+        Self {
+            len: 0,
+            start: 0,
+            end: 0,
+            verts: [""; 32],
+            edges: [EMPTY; 32],
+            kinds: [Kind::End; 32],
+        }
+    }
+
+    /// Retrieve a vertex's index, adding it to the graph if necessary.
+    pub fn vert(&mut self, key: &'a str) -> usize {
+        match self.verts.iter().position(|vert| *vert == key) {
+            Some(i) => i,
+            None => {
+                let i = self.len;
+                self.verts[i] = key;
+                self.len += 1;
+                let kind = Kind::new(key);
+                if kind == Kind::Start {
+                    self.start = i;
+                }
+                if kind == Kind::End {
+                    self.end = i;
+                }
+                self.kinds[i] = kind;
+                i
+            }
+        }
+    }
+
+    /// Create an undirected edge between the two vertex indices.
+    pub fn edge(&mut self, lhs: usize, rhs: usize) {
+        self.edges[lhs].push(rhs);
+        self.edges[rhs].push(lhs);
+    }
+
+    #[inline]
+    /// Return the kind of vertex at this index.
+    pub fn kind(&self, key: usize) -> Kind {
+        self.kinds[key]
+    }
+
+    /// Yield a vertex's neighbors.
+    pub fn neighbors(&'a self, key: usize) -> impl Iterator<Item = &'a usize> {
+        self.edges[key].iter()
+    }
+}
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+/// The kind of node at an index in the graph.
+pub enum Kind {
+    /// Start node.
+    Start,
+    /// End node.
+    End,
+    /// Small "cave", one whose key was all lower-case.
+    Small,
+    /// Big "cave", one whose key was not all lower-case.
+    Big,
+}
+
+impl Kind {
+    /// Determine a node's kind.
+    pub fn new(s: &str) -> Self {
+        match s {
+            "start" => Kind::Start,
+            "end" => Kind::End,
+            _ if s.to_lowercase() == s => Kind::Small,
+            _ => Kind::Big,
+        }
+    }
 }
 
 #[derive(PartialEq, Eq, Debug)]
@@ -64,51 +147,54 @@ pub enum Mode {
 
 #[derive(PartialEq, Eq, Debug)]
 /// A path through the graph.
-///
-/// Provides stack-oriented modification with _O(1)_ membership testing.
-pub struct Path<'a> {
-    path: Vec<&'a str>,
-    counts: HashMap<&'a str, usize>,
+pub struct Path {
+    len: usize,
+    path: [usize; 32],
 }
 
-impl<'a> Path<'a> {
+impl Path {
     /// Create an empty path.
     pub fn new() -> Self {
         Self {
-            path: Vec::new(),
-            counts: HashMap::new(),
-        }
-    }
-
-    /// Push a node onto the end of the path.
-    fn push(&mut self, key: &'a str) {
-        self.path.push(key);
-        *self.counts.entry(key).or_insert(0) += 1;
-    }
-
-    /// Pop the last node from the end of the path.
-    fn pop(&mut self) {
-        if let Some(key) = self.path.pop() {
-            self.counts.entry(key).and_modify(|n| *n -= 1);
+            len: 0,
+            path: [0; 32],
         }
     }
 
     #[inline]
+    /// Push a node onto the end of the path.
+    fn push(&mut self, key: usize) {
+        self.path[self.len] = key;
+        self.len += 1;
+    }
+
+    #[inline]
+    /// Pop the last node from the end of the path.
+    fn pop(&mut self) {
+        self.len -= 1;
+    }
+
+    #[inline]
     /// Count the number of times a node appears in the path.
-    fn occurrences(&self, key: &'a str) -> usize {
-        *self.counts.get(key).unwrap_or(&0)
+    fn occurrences(&self, key: usize) -> usize {
+        self.path[0..self.len]
+            .iter()
+            .filter(|node| **node == key)
+            .count()
     }
 }
 
-fn parse_graph<'a>(lines: &'a [String]) -> HashMap<&'a str, Vec<&'a str>> {
-    lines
-        .iter()
-        .flat_map(|line| line.split_once('-'))
-        .flat_map(|(lhs, rhs)| [(lhs, rhs), (rhs, lhs)])
-        .fold(HashMap::new(), |mut graph, (lhs, rhs)| {
-            graph.entry(lhs).or_default().push(rhs);
+fn parse_graph<'a>(lines: &'a [String]) -> Graph<'a> {
+    lines.iter().flat_map(|line| line.split_once('-')).fold(
+        Graph::new(),
+        |mut graph, (lhs, rhs)| {
+            let lhs = graph.vert(lhs);
+            let rhs = graph.vert(rhs);
+            graph.edge(lhs, rhs);
             graph
-        })
+        },
+    )
 }
 
 check!(ex 1 = 10, ex 2 = 36, part 1 = 3761, part 2 = 99138);
+bench!(part 1, part 2);
