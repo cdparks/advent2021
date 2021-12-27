19bca3f88c7715e5f49c33b979789e045e933b30
Chris Parks <christopher.daniel.parks@gmail.com>
2021-12-27 12:10:43 -0800

    Generate gif for day 25

--- advent/src/day25.rs
+++ advent/src/day25.rs
@@ -1,3 +1,10 @@
+use image::codecs::gif::GifEncoder;
+use image::imageops::{resize, FilterType};
+use image::{Delay, Frame, Rgba, RgbaImage};
+use itertools::Itertools;
+use std::fs::File;
+use std::time::Duration;
+
 /// Find the step where no more moves are possible.
 pub fn part1(lines: &[String]) -> usize {
     solve(parse(lines))
@@ -22,14 +29,48 @@ fn parse(lines: &[String]) -> Vec<Vec<Facing>> {
 /// Iterate the map until no more moves are possible.
 fn solve(mut map: Vec<Vec<Facing>>) -> usize {
     let mut steps = 0;
+    let mut images = Vec::new();
     loop {
         let east = step(&mut map, Facing::East, (0, 1));
         let south = step(&mut map, Facing::South, (1, 0));
+        images.push(paint(&map));
         steps += 1;
         if !east && !south {
             break;
         }
     }
+    const SCALE: u32 = 5;
+    const SAMPLE: usize = 4;
+    let mut width = map[0].len() as u32;
+    let mut height = map.len() as u32;
+    println!("{} {} x {} images", images.len(), width, height,);
+    width *= SCALE;
+    height *= SCALE;
+    let frames = images
+        .iter()
+        .rev()
+        .enumerate()
+        .filter(|(i, _)| *i == 0 || *i % SAMPLE == 0)
+        .rev()
+        .map(|(_, image)| {
+            let resized = resize(image, width, height, FilterType::Nearest);
+            Frame::from_parts(
+                resized,
+                0,
+                0,
+                Delay::from_saturating_duration(Duration::from_millis(1)),
+            )
+        })
+        .collect_vec();
+    println!(
+        "{} {} x {} filtered resized frames",
+        frames.len(),
+        width,
+        height
+    );
+    let gif = File::create("day25.gif").unwrap();
+    let mut encoder = GifEncoder::new(gif);
+    encoder.encode_frames(frames).unwrap();
     steps
 }
 
@@ -56,6 +97,24 @@ fn step(map: &mut Vec<Vec<Facing>>, direction: Facing, change: (usize, usize)) -
         map[i][j] = Facing::Empty;
     }
     changed
+}
+
+fn paint(map: &Vec<Vec<Facing>>) -> RgbaImage {
+    let width = map[0].len() as u32;
+    let height = map.len() as u32;
+    let mut buffer = RgbaImage::new(width, height);
+    for y in 0..height {
+        for x in 0..width {
+            let i = y as usize;
+            let j = x as usize;
+            match map[i][j] {
+                Facing::Empty => buffer.put_pixel(x, y, Rgba([0, 0, 0, 1])),
+                Facing::East => buffer.put_pixel(x, y, Rgba([255, 0, 64, 1])),
+                Facing::South => buffer.put_pixel(x, y, Rgba([0, 64, 255, 1])),
+            }
+        }
+    }
+    buffer
 }
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
